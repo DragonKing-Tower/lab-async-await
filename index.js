1d594b079f964bb19d89e565316e4458be148cc6
Cyril Rafalovich <replanttrees@gmail.com>
2025-05-21 15:41:38 -1000

    This is my submission for the 5th lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here!
+async function fetchFunction() {
+	try {
+		const site = await fetch("https://jsonplaceholder.typicode.com/posts");
+		const data = await site.json();
+		displayPosts(data);
+	} catch (error) {
+		console.error("Error fetching posts:", error);
+	}
+}
+
+function displayPosts(data) {
+	const postList = document.querySelector("#post-list");
+	data.forEach((book) => {
+		const container = document.createElement("li"); //make container for each book
+		const titleDoc = document.createElement("h1");
+		titleDoc.textContent = book.title; //make title in html element for book
+		const bodyDoc = document.createElement("p");
+		bodyDoc.textContent = book.body; //make body in html element for book
+		container.append(titleDoc, bodyDoc); //place elements in container
+		postList.append(container); //attach container to existing DOM
+	});
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	fetchFunction();
+});
